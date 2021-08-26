b2c41ed668a248a3a635ecb607d62a59f1bef7be
Adam Bundschuh <bundschuh.adam@gmail.com>
2021-08-26 18:38:55 -0400

    Added start of expense filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,32 +1,40 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem.js";
+import ExpenseFilter from "../ExpenseFilter/ExpenseFilter.js";
 import Card from "../UI/Card.js";
 
 const Expenses = (props) => {
+  const SelectYearHandler = (yearData) => {
+    console.log(yearData);
+  };
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.expense[0].title}
-        amount={props.expense[0].amount}
-        date={props.expense[0].date}
-      />
-      <ExpenseItem
-        title={props.expense[1].title}
-        amount={props.expense[1].amount}
-        date={props.expense[1].date}
-      />
-      <ExpenseItem
-        title={props.expense[2].title}
-        amount={props.expense[2].amount}
-        date={props.expense[2].date}
-      />
-      <ExpenseItem
-        title={props.expense[3].title}
-        amount={props.expense[3].amount}
-        date={props.expense[3].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter onSelectYear={SelectYearHandler} />
+        <ExpenseItem
+          title={props.expense[0].title}
+          amount={props.expense[0].amount}
+          date={props.expense[0].date}
+        />
+        <ExpenseItem
+          title={props.expense[1].title}
+          amount={props.expense[1].amount}
+          date={props.expense[1].date}
+        />
+        <ExpenseItem
+          title={props.expense[2].title}
+          amount={props.expense[2].amount}
+          date={props.expense[2].date}
+        />
+        <ExpenseItem
+          title={props.expense[3].title}
+          amount={props.expense[3].amount}
+          date={props.expense[3].date}
+        />
+      </Card>
+    </div>
   );
-}
+};
 
 export default Expenses;
--- src/components/NewExpense/NewExpenseForm.js
+++ src/components/NewExpense/NewExpenseForm.js
@@ -19,6 +19,7 @@ const NewExpenseForm = (props) => {
   };
 
   const submitHandler = (event) => {
+    
     event.preventDefault();
 
     const expenseData = {
